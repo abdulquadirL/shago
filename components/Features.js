6efaeeaa45bf2093b78f52798b5b6510b75ec7d7
buharry <elanquad92@gmail.com>
2023-07-12 16:01:39 +0100

    Add Box, buttons and when hovered will hide/show text

--- components/Features.js
+++ components/Features.js
@@ -1,23 +1,49 @@
-import { Box, Flex, Button, VStack, Text } from '@chakra-ui/react'
+import { Box, Flex, HStack, Button, Text } from '@chakra-ui/react'
 import React from 'react'
 //import Image  from 'next/image'
+import { useState } from 'react';
 
 export const Features = () => {
+  const [isTextVisible, setIsTextVisible] = useState(false);
+    const handleButtonHover = () => {
+      setIsTextVisible(!isTextVisible);
+    };
   return (
     <Box
-    bg="blue.200" 
-    as='section'
+    bg="blue.300"
     >
-    <Box ml="20px">
-      <Flex flexWrap='wrap'>   
-      <VStack mb='30px' >
-      <Text color='black' fontSize='25px' mt='10px' mb='10px' fontWeight='bold'>Features</Text>
-        <Button borderRadius='10px'  bg='blue.500'  mb='10px'>Store Layout</Button>
-        <Button variant="ghost" bg='brown'  mb='25px'>Stocks Management</Button>
-        <Button bg='purple.500' mt='15px'>Sales & Invoicing</Button>
-        <Button bg='orange' mt='25px'>Credit/Debit management</Button>
-        <Button bg='black'  mb='25px'>More...</Button>
-      </VStack>
+
+    <Box
+     as='section'
+     ml="20px"
+    >
+      <Text as="H1" color='black' fontSize='25px' mt='10px' mb='10px' fontWeight='bold'>Features</Text>
+      <Flex
+       flexWrap='wrap'
+      >   
+      <HStack mb='30px' >
+        <Box width="200px" height="200px" pl="5px" pr="5px">
+          <Button borderRadius='10px'  bg='blue.500'  mr='20px' onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Multiple Shop Access</Button >
+          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
+        </Box>
+        <Box width="200px" height="200px" pl="5px" pr="5px">
+          <Button  variant="ghost" bg='brown'  mr='20px' onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Stocks Management</Button >
+          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
+        </Box>
+        <Box width="200px" height="200px"  alignContent="center" pl="5px" pr="5px">
+          <Button mr="20px" bg='violet' onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Invoice </Button>
+          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
+        </Box>
+        <Box width="200px" height="200px" pl="5px" pr="5px">
+          <Button mr="20px" bg='gold' onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Sales Report</Button>
+          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
+        </Box>
+        <Box width="200px" height="200px" pl="5px" pr="5px">
+          <Button bg='orange' mr='20px'onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Credit/Debt management</Button>
+          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
+        </Box>
+        
+      </HStack>
       </Flex>
       </Box> 
     </Box>
