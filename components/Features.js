9dec2cc27036305fc68b6172ee41d06df9691787
buharry <elanquad92@gmail.com>
2023-07-12 17:14:25 +0100

    redesign the hover action

--- components/Features.js
+++ components/Features.js
@@ -5,48 +5,61 @@ import { useState } from 'react';
 
 export const Features = () => {
   const [isTextVisible, setIsTextVisible] = useState(false);
-    const handleButtonHover = () => {
-      setIsTextVisible(!isTextVisible);
-    };
+  const handleButtonHover = () => {
+    setIsTextVisible(!isTextVisible);
+  };
+  const [isTextVisiblew, setIsTextVisiblew] = useState(false);
+  const handleButtonHoverw = () => {
+    setIsTextVisiblew(!isTextVisiblew);
+  };
+  const [isTextVisiblex, setIsTextVisiblex] = useState(false);
+  const handleButtonHoverx = () => {
+    setIsTextVisiblex(!isTextVisiblex);
+  };
+  const [isTextVisibley, setIsTextVisibley] = useState(false);
+  const handleButtonHovery = () => {
+    setIsTextVisibley(!isTextVisibley);
+  };
+  const [isTextVisiblez, setIsTextVisiblez] = useState(false);
+  const handleButtonHoverz = () => {
+    setIsTextVisiblez(!isTextVisiblez);
+  };
   return (
     <Box
-    bg="blue.300"
-    >
+     bg="blue.300"
+     px='15px'
+     py="15px"
 
-    <Box
-     as='section'
-     ml="20px"
     >
-      <Text as="H1" color='black' fontSize='25px' mt='10px' mb='10px' fontWeight='bold'>Features</Text>
+      <Text as="H1" color='black' fontSize='25px' mt='10px' mb='10px' fontzeight='bold'>Features</Text>
       <Flex
        flexWrap='wrap'
       >   
       <HStack mb='30px' >
-        <Box width="200px" height="200px" pl="5px" pr="5px">
-          <Button borderRadius='10px'  bg='blue.500'  mr='20px' onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Multiple Shop Access</Button >
+        <Box width="250px" height="200px" pl="5px" pr="5px">
+          <Button borderRadius='10px'  bg='blue.500'  mr='20px' onMouseEnter={handleButtonHoverw} onMouseLeave={handleButtonHoverw}>Multiple Shop Access</Button >
+          {isTextVisiblew && <Text color="white">Manage all your shops in one platform</Text>}
+        </Box>
+        <Box width="250px" height="200px" pl="5px" pr="5px">
+          <Button borderRadius='10px'  variant="ghost" bg='brown'  mr='20px' width="200px" onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Stocks Management</Button >
           {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
         </Box>
-        <Box width="200px" height="200px" pl="5px" pr="5px">
-          <Button  variant="ghost" bg='brown'  mr='20px' onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Stocks Management</Button >
-          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
+        <Box width="250px" height="200px"  alignContent="center" pl="5px" pr="5px">
+          <Button borderRadius='10px' mr="20px" bg='violet' width="200px" onMouseEnter={handleButtonHoverx} onMouseLeave={handleButtonHoverx}>Invoice </Button>
+          {isTextVisiblex && <Text color="white">Manage all your shops in one platform</Text>}
         </Box>
-        <Box width="200px" height="200px"  alignContent="center" pl="5px" pr="5px">
-          <Button mr="20px" bg='violet' onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Invoice </Button>
-          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
+        <Box width="250px" height="200px" pl="5px" pr="5px">
+          <Button borderRadius='10px' mr="20px" bg='gold' width="200px" onMouseEnter={handleButtonHovery} onMouseLeave={handleButtonHovery}>Sales Report</Button>
+          {isTextVisibley && <Text color="white">Manage all your shops in one platform</Text>}
         </Box>
-        <Box width="200px" height="200px" pl="5px" pr="5px">
-          <Button mr="20px" bg='gold' onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Sales Report</Button>
-          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
-        </Box>
-        <Box width="200px" height="200px" pl="5px" pr="5px">
-          <Button bg='orange' mr='20px'onMouseEnter={handleButtonHover} onMouseLeave={handleButtonHover}>Credit/Debt management</Button>
-          {isTextVisible && <Text color="white">Manage all your shops in one platform</Text>}
+        <Box width="250px" height="200px" pl="5px" pr="5px">
+          <Button borderRadius='10px' bg='orange' mr='20px' width="200px" onMouseEnter={handleButtonHoverz} onMouseLeave={handleButtonHoverz}>Credit/Debt management</Button>
+          {isTextVisiblez && <Text color="white">Manage all your shops in one platform</Text>}
         </Box>
         
       </HStack>
       </Flex>
       </Box> 
-    </Box>
     
   )
 }
