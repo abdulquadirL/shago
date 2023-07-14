0d00af11a83ee9d7bc1dcb26983b46e10379ed44
buharry <elanquad92@gmail.com>
2023-07-14 14:54:16 +0100

    changed Plans button styles to action button

--- components/Pricing.js
+++ components/Pricing.js
@@ -1,70 +1,79 @@
 import React from 'react'
-import { Box, Button, Flex, Text, HStack } from '@chakra-ui/react'
+import { Box, Button, Flex, Text, HStack, Spacer } from '@chakra-ui/react'
 
 export const Pricing = () => {
   return (
     <Box
+     width="100%"
      height='360px'  
-     bg='green' 
+     bg='blue.600' 
      mt='20px' 
-     px='15px' 
+     px={["10px", "15px", '25px']} 
      py="15px"
       
     >
-
+      
       <Text
        as="H1"
        fontFamily='sans-serif' 
-       fontSize='25px' 
+       fontSize={["10px", "15px", '25px']}  
        color='white' 
        fontWeight='semibold'
+       mb="20px"
       > Pricing</Text>
-        
-      <HStack>
+       <HStack>
       <Box as="section"
+      bg="whiteAlpha.300"
            mr='20px'
-           width="200px"
+           width= {["120px", "200px", "300px"]}
            height="300px"
-           px="20px"
+           px='20px'
            py="20px"
+           
       >
       <Button
-         bg='gray.200' 
-         color='blue.300'   
-         width='150px'  
-         fontSize='22px'
+          bg="orange.300"
+          width="100%"   
+          mb='10px'  
+          fontSize={["xl", "xl", "22px"]} 
          >
         Basic 
       </Button>
-      <p color='white'>NGN2500/Monthly</p> 
+      <Text color='white'>NGN2500/Monthly</Text> 
       </Box> 
-      <Box as='section' mr='20px'  width="200px" height="300px" px="20px"
-           py="20px">
-      <Button
-       bg='gray.200' 
-       color='blue.300' 
-       mb="10px"
-       mr='20px' 
-       width='150px'  
-       fontSize='22px'>
+      <Spacer/>
+      <Box as="section"
+           bg="whiteAlpha.300"
+           mr={["10px", "15px", "20px"]}
+           width={["120px", "200px", "300px"]}
+           height="300px"
+           px={["10px", "15px", "20px"]}
+           py={["10px", "15px", "20px"]}
+           alignItems="center">
+           
+      <Button 
+       bg="orange.300"
+       width="100%" 
+       mb="10px"  
+       fontSize={["xl", "xl", "22px"]}
+       >
         Ultimate  
       </Button>
-       <p color='white'>NGN5000/Monthly</p>
+       <Text color='white'>NGN5000/Monthly</Text>
     </Box>
-    <Box as='section' mr='20px' width="200px" height="300px" px="20px"
+    <Spacer/>
+    <Box as='section' bg="whiteAlpha.300" mr='20px' width={["120px", "200px", "300px"]} height="300px" px="20px"
            py="20px">  
       <Button
-       bg='gray.200' 
-       color='blue.300' 
-       mb='10px' 
-       mr='20px' 
-       width='150px' 
-       fontSize='22px'>
+       bg="orange.300"
+       width="100%"   
+       mb='10px'  
+       fontSize={["xl", "xl", "22px"]} 
+       >
         Premium  
       </Button>
       </Box>
-      </HStack>
-      
+       </HStack> 
     </Box>
   )
 }
